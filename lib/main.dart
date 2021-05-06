8ed443bc70e75e743a4236a4a84480e5024bcb05
Abdul Hanan <hanankhanajk@gmail.com>
2021-05-06 16:02:28 +0500

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'Kalimatic.dart';
+
+void main() {
+  runApp(
+      new MaterialApp(
+        title: 'Klimatic',
+        home: new Klimatic(),
+      )
+  );
+}
---
+++ lib/utill/utills.dart
@@ -0,0 +1,2 @@
+final appId="afaf5a6b62ede24faa601c661d520cb3";
+final defaultCity="Pakistan";
